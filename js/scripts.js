953f83ee92cb6b09cffe9e2985310d0db909fb1b
MacarenaCarrero <macarena.c.rios@gmail.com>
2024-10-19 20:55:05 +0200

    ejercicios string

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const word= "Macarena";
+console.log(word.toUpperCase(word.length >5));
+console.log(word.toLowerCase(word.length <5));
+
+const rLetter= "Macarena";
+console.log(rLetter.charAt(Math.random()*9));
+
+const compared= ("corta", "larguisima");
+console.log(compared.length("corta" > "larguisima") );
