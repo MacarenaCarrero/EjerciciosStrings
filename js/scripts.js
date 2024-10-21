72a6b1d81ac40d946391d12f8aed6450fea4dce0
Diegohhallam <macarena.c.rios@gmail.com>
2024-10-21 18:50:56 +0200

    EjerciciosStrings

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,135 @@
-const word= "Macarena";
-console.log(word.toUpperCase(word.length >5));
-console.log(word.toLowerCase(word.length <5));
+//primero
+const word = "Macarena";
+console.log(word.toUpperCase(word.length > 5));
+console.log(word.toLowerCase(word.length < 5));
 
-const rLetter= "Macarena";
-console.log(rLetter.charAt(Math.random()*9));
+//segundo
+function randomLetter(word) {
+  const randomPosition = Math.floor(Math.random() * word.length);
 
-function compareWords(word1, word2){ 
-    if (word1.length > word2.length){
-        return "word1"
-    }else if (word1.length < word2.length){
-        return "word2"
-    }else{
-        return "Tienen la misma longitud"
-    }
+  console.log(word.charAt(randomPosition));
 }
 
-const compare=("corta", "larguisima")
-const compare2=("larguita", "cor")
-const compare3=("mac", "cam")
+randomLetter("Macarena");
+randomLetter("Ordenador");
 
-console.log(compare)
-console.log(compare2)
-console.log(compare3)
+//tercero
+function compareWords(word1, word2) {
+  if (word1.length > word2.length) {
+    return word1;
+  } else if (word1.length < word2.length) {
+    return word2;
+  } else {
+    return "Tienen la misma longitud";
+  }
+}
 
-// he probado a ponerlo arriba y abajo y no me sale la opcion de las 2 son iguales
+const compare = compareWords("corta", "larguisima");
+const compare2 = compareWords("larguita", "cor");
+const compare3 = compareWords("mac", "cam");
 
+console.log(compare);
+console.log(compare2);
+console.log(compare3);
 
+//cuarto
 
+function generateWord(word1, word2, word3) {
+  const randomPositionWord1 = Math.floor(Math.random() * word1.length);
+  const randomPosition2Word1 = Math.floor(Math.random() * word1.length);
+
+  const randomPositionWord2 = Math.floor(Math.random() * word2.length);
+  const randomPosition2Word2 = Math.floor(Math.random() * word2.length);
+
+  const randomPositionWord3 = Math.floor(Math.random() * word3.length);
+  const randomPosition2Word3 = Math.floor(Math.random() * word3.length);
+
+  console.log(
+    word1.charAt(randomPositionWord1) +
+      word1.charAt(randomPosition2Word1) +
+      word2.charAt(randomPositionWord2) +
+      word2.charAt(randomPosition2Word2) +
+      word3.charAt(randomPositionWord3) +
+      word3.charAt(randomPosition2Word3)
+  );
+}
+
+const resultWord = generateWord("Hola", "macarena", "adios");
+const resultWord2 = generateWord("Alfredo", "Macarena", "Siena");
+
+//quinto
+
+function checkDomain(email) {
+  const atPosition = email.indexOf("@");
+  const userName = email.substring(0, atPosition);
+  const domain = email.substring(atPosition + 1);
+
+  console.log("El usuario es " + userName);
+  console.log("El dominio es " + domain);
+}
+
+checkDomain("macarena@gmail.com");
+
+//sexto
+
+function capitalLetters(word) {
+  const capitalL =
+    word.charAt(0).toUpperCase() +
+    word.charAt(1).toLowerCase() +
+    word.charAt(3).toUpperCase() +
+    word.charAt(4).toLowerCase();
+
+  console.log(capitalL);
+}
+
+capitalLetters("adios");
+
+//seven
+
+function userName(name, surnames) {
+  const name1 = name.charAt(0) + surnames;
+  const number = Math.round(Math.random() * 101);
+
+  console.log(name1 + number);
+}
+
+userName("Macarena", "Carrero");
+
+//ocho
+
+function invertedWords(word) {
+  const order =
+    word.charAt(3) + word.charAt(2) + word.charAt(1) + word.charAt(0);
+
+  console.log(order);
+}
+
+invertedWords("maca");
+
+//nueve
+
+function wordsSize(word1, word2, word3) {
+  const addition = word1.length + word2.length + word3.length;
+
+  console.log(addition);
+}
+
+wordsSize("hola", "siguiente", "adios");
+
+//diez
+
+function titleFormat(title) {
+  const capitalLetters = title.charAt(0).toUpperCase();
+  const spacePosition = title.indexOf(" ");
+  const capitalLetters2 = title.charAt(spacePosition + 1).toUpperCase();
+  const restFirstWord = title.substring(1, spacePosition);
+  const restSecondWord = title.substring(spacePosition + 2);
+
+  console.log(
+    capitalLetters + restFirstWord + " " + capitalLetters2 + restSecondWord
+  );
+}
+titleFormat("Érase una");
+titleFormat("hola macarena");
+
+//once
